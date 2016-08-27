6a662a14ddfeeb28c891c6aa7a7401010418daba
jet_vorobev <ia.vorobev@jet.msk.su>
2016-08-27 23:23:55 +0300

    hw0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,11 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /**
  * GKislin
@@ -29,7 +29,36 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        Map<LocalDate, Boolean> exceedMap = getDatesWithExceeded(mealList, caloriesPerDay);
+        List<UserMealWithExceed> resultList = new ArrayList<>();
+        for (UserMeal meal : mealList) {
+            meal.getCalories();
+            LocalTime mealTime = meal.getDateTime().toLocalTime();
+            if (TimeUtil.isBetween(mealTime, startTime, endTime)) {
+                resultList.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(),
+                        meal.getCalories(), exceedMap.get(meal.getDateTime().toLocalDate())));
+            }
+        }
+        return resultList;
+    }
+
+    private static Map<LocalDate, Boolean> getDatesWithExceeded(List<UserMeal> mealList, int caloriesPerDay) {
+        Map<LocalDate, Boolean> exceedMap = new TreeMap<>();
+        LocalDate mealDate = mealList.get(0).getDateTime().toLocalDate();
+        int totalCalories = 0;
+        boolean exceeded = false;
+        for (UserMeal meal : mealList) {
+            if (!meal.getDateTime().toLocalDate().equals(mealDate)) {
+                mealDate = meal.getDateTime().toLocalDate();
+                totalCalories = 0;
+                exceeded = false;
+            }
+            totalCalories += meal.getCalories();
+            if (totalCalories > caloriesPerDay) {
+                exceeded = true;
+            }
+            exceedMap.put(mealDate, exceeded);
+        }
+        return exceedMap;
     }
 }
